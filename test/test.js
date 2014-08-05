7de1e7eeb223c16ff37693db49b0ecd466fc2848
Phil Hughes <theephil@gmail.com>
2014-08-05 16:10:19 +0100

    - Added some tests

---
+++ test/test.js
@@ -0,0 +1,26 @@
+QUnit.test("Show off-canvas menu test", function (assert) {
+    $("#offcanvas-toggle").trigger("click");
+
+    assert.ok($("#js-bootstrap-offcanvas").hasClass("in"), "Off-Canvas should be visible");
+});
+
+QUnit.test("Hide off-canvas menu test", function (assert) {
+    $("#offcanvas-toggle").trigger("click");
+
+    assert.ok(!$("#js-bootstrap-offcanvas").hasClass("in"), "Off-Canvas should be hidden");
+});
+
+QUnit.test("Body click hide off-canvas menu test", function (assert) {
+    $("#offcanvas-toggle").trigger("click");
+    assert.ok($("#js-bootstrap-offcanvas").hasClass("in"), "Off-Canvas should be visible");
+
+    $("body").trigger("click");
+    assert.ok(!$("#js-bootstrap-offcanvas").hasClass("in"), "Off-Canvas should be hidden");
+});
+
+QUnit.test("Dropdown test", function (assert) {
+    $("#js-dropdown-test").trigger("click");
+
+    assert.ok($("#js-dropdown-test").parent().hasClass("active"), "Dropdown menu should be visible");
+    assert.ok($("#js-dropdown-test").parent().find(".dropdown-menu").hasClass("shown"), "Dropdown menu should be hidden");
+});
