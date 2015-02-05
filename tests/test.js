882c0e7da22966bc9491ddc70892b35e82e5a459
Phil Hughes <me@iamphill.com>
2015-02-05 15:41:31 +0000

    Updated tests to check offsets for transforms

--- tests/test.js
+++ tests/test.js
@@ -1,26 +1,35 @@
-test("Show off-canvas menu test", function (assert) {
-    $("#offcanvas-toggle").trigger("click");
+(function ($) {
+    test("Show off-canvas menu test", function (assert) {
+        assert.expect(1);
+        $("#offcanvas-toggle").trigger("click");
 
-    assert.ok($("#js-bootstrap-offcanvas").hasClass("in"), "Off-Canvas should be visible");
-});
+        assert.ok($("#js-bootstrap-offcanvas").hasClass("in"), "Off-Canvas should be visible");
+    });
 
-test("Hide off-canvas menu test", function (assert) {
-    $("#offcanvas-toggle").trigger("click");
+    test("Hide off-canvas menu test", function (assert) {
+        assert.expect(1);
+        $("#offcanvas-toggle").trigger("click");
 
-    assert.ok($("#js-bootstrap-offcanvas").is(":not(in)"), "Off-Canvas should be hidden");
-});
+        assert.ok($("#js-bootstrap-offcanvas").is(":not(in)"), "Off-Canvas should be hidden");
+    });
 
-test("Body click hide off-canvas menu test", function (assert) {
-    $("#offcanvas-toggle").trigger("click");
-    assert.ok($("#js-bootstrap-offcanvas").hasClass("in"), "Off-Canvas should be visible");
+    test("Body click hide off-canvas menu test", function (assert) {
+        assert.expect(4);
 
-    $("body").trigger("click");
-    assert.ok(!$("#js-bootstrap-offcanvas").hasClass("in"), "Off-Canvas should be hidden");
-});
+        $("#offcanvas-toggle").trigger("click");
+        assert.ok($("#js-bootstrap-offcanvas").hasClass("in"), "Off-Canvas should be visible");
+        assert.deepEqual($("#js-bootstrap-offcanvas").offset().left, 0, 'Off-Canvas offset should be 0 (Visible)');
 
-test("Dropdown test", function (assert) {
-    $("#js-dropdown-test").trigger("click");
+        $("body").trigger("click");
+        assert.ok(!$("#js-bootstrap-offcanvas").hasClass("in"), "Off-Canvas should be hidden");
+        assert.deepEqual($("#js-bootstrap-offcanvas").offset().left, -250, 'Off-Canvas offset should be -250 (Hidden)');
+    });
 
-    assert.ok($("#js-dropdown-test").parent().hasClass("active"), "Dropdown menu should be visible");
-    assert.ok($("#js-dropdown-test").parent().find(".dropdown-menu").hasClass("shown"), "Dropdown menu should be hidden");
-});
+    test("Dropdown test", function (assert) {
+        assert.expect(2);
+        $("#js-dropdown-test").trigger("click");
+
+        assert.ok($("#js-dropdown-test").parent().hasClass("active"), "Dropdown menu should be visible");
+        assert.ok($("#js-dropdown-test").parent().find(".dropdown-menu").hasClass("shown"), "Dropdown menu should be hidden");
+    });
+})(jQuery);
