948a1671d649588ebca6cc87e40c101fd7ceb9a4
Phil Hughes <me@iamphill.com>
2018-06-08 22:15:09 +0100

    test update :pray:

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,72 +1,13 @@
-const fs = require('fs');
+import fs from 'fs';
+import path from 'path';
 import puppeteer from 'puppeteer';
 import { startServer } from 'polyserve';
 import pixelmatch from 'pixelmatch';
 import { PNG } from 'pngjs';
-import util from 'util';
 
-const exec = util.promisify(require('child_process').exec);
 const port = 8080;
-const testDir = './tmp/screenshots';
-const goldenDir = './__tests__/screenshots';
-
-function compareScreenshots(fileName) {
-  return new Promise((resolve, reject) => {
-    const img1 = fs
-      .createReadStream(`${testDir}/${fileName}.png`)
-      .pipe(new PNG())
-      .on('parsed', doneReading);
-    const img2 = fs
-      .createReadStream(`${goldenDir}/${fileName}.png`)
-      .pipe(new PNG())
-      .on('parsed', doneReading);
-
-    let filesRead = 0;
-    function doneReading() {
-      // Wait until both files are read.
-      if (++filesRead < 2) return;
-
-      // The files should be the same size.
-      expect(img1.width).toBe(img2.width);
-      expect(img1.height).toBe(img2.height);
-
-      // Do the visual diff.
-      const diff = new PNG({
-        width: img1.width,
-        height: img2.height,
-      });
-      const numDiffPixels = pixelmatch(
-        img1.data,
-        img2.data,
-        diff.data,
-        img1.width,
-        img1.height,
-        { threshold: 0.1 }
-      );
-
-      // The files should look the same.
-      expect(numDiffPixels).toBe(0);
-      resolve();
-    }
-  });
-}
-
-async function takeAndCompareScreenshot(page, route, filePrefix = '') {
-  // If you didn't specify a file, use the name of the route.
-  const fileName = `${filePrefix}-${route}`;
-
-  // Start the browser, go to that page, and take a screenshot.
-  await page.goto(`http://127.0.0.1:${port}/${route}`);
-
-  await page.click('.navbar-toggler');
-
-  await page.screenshot({
-    path: `${testDir}/${fileName}.png`,
-  });
-
-  // Test to see if it's right.
-  return compareScreenshots(fileName);
-}
+const currentDir = './tmp/screenshots';
+const baselineDir = './__tests__/screenshots';
 
 describe('screenshots are correct', () => {
   let polyserve, browser, page;
@@ -74,6 +15,8 @@ describe('screenshots are correct', () => {
   beforeAll(async function() {
     polyserve = await startServer({
       port,
+      root: path.join(__dirname, '..'),
+      moduleResolution: 'node',
     });
 
     if (!fs.existsSync('./tmp')) fs.mkdirSync('./tmp');
@@ -96,6 +39,75 @@ describe('screenshots are correct', () => {
   afterEach(() => browser.close());
 
   it('opens offcanvas menu', async () => {
-    await takeAndCompareScreenshot(page, 'example', 'open');
+    await takeAndCompareScreenshot(
+      () => page.click('.navbar-toggler'),
+      page,
+      'example',
+      'open'
+    );
   });
 });
+
+async function takeAndCompareScreenshot(action, page, route, filePrefix) {
+  // If you didn't specify a file, use the name of the route.
+  let fileName = filePrefix + '-' + (route ? route : 'index');
+
+  await page.goto(`http://127.0.0.1:${port}/${route}`);
+
+  await action();
+
+  await page.screenshot({ path: `${currentDir}/${fileName}.png` });
+
+  return compareScreenshots(fileName);
+}
+
+function compareScreenshots(view) {
+  return new Promise((resolve, reject) => {
+    const img1 = fs
+      .createReadStream(`${currentDir}/${view}.png`)
+      .pipe(new PNG())
+      .on('parsed', doneReading);
+    const img2 = fs
+      .createReadStream(`${baselineDir}/${view}.png`)
+      .pipe(new PNG())
+      .on('parsed', doneReading);
+
+    let filesRead = 0;
+    function doneReading() {
+      // Wait until both files are read.
+      if (++filesRead < 2) return;
+
+      // The files should be the same size.
+      expect(img1.width).toEqual(img2.width);
+      expect(img1.height).toEqual(img2.height);
+
+      // Do the visual diff.
+      const diff = new PNG({ width: img1.width, height: img1.height });
+
+      // Skip the bottom/rightmost row of pixels, since it seems to be
+      // noise on some machines :/
+      const width = img1.width - 1;
+      const height = img1.height - 1;
+
+      const numDiffPixels = pixelmatch(
+        img1.data,
+        img2.data,
+        diff.data,
+        width,
+        height,
+        { threshold: 0.2 }
+      );
+      const percentDiff = (numDiffPixels / (width * height)) * 100;
+
+      const stats = fs.statSync(`${currentDir}/${view}.png`);
+      const fileSizeInBytes = stats.size;
+      console.log(
+        `📸 ${view}.png => ${fileSizeInBytes} bytes, ${percentDiff}% different`
+      );
+
+      //diff.pack().pipe(fs.createWriteStream(`${currentDir}/${view}-diff.png`));
+      expect(numDiffPixels).toEqual(0);
+      resolve();
+    }
+  });
+}
